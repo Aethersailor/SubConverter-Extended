3c0a3a044d3b3890f94dfbca227aebe35f5cb824
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2026-07-29 14:01:50 +0800

    feat: make bundled COCR assets policy-driven

--- src/handler/custom_openclash_rules_endpoint.h
+++ src/handler/custom_openclash_rules_endpoint.h
@@ -1,11 +1,26 @@
 #ifndef CUSTOM_OPENCLASH_RULES_ENDPOINT_H_INCLUDED
 #define CUSTOM_OPENCLASH_RULES_ENDPOINT_H_INCLUDED
 
+#include <map>
+#include <set>
 #include <string>
 
 #include "server/webserver.h"
 
 namespace custom_openclash_rules_endpoint {
+
+struct DirectoryPageSnapshot {
+  std::string content;
+  std::string etag;
+};
+
+using DirectoryIndexSnapshot =
+    std::map<std::string, DirectoryPageSnapshot>;
+
+bool buildDirectoryIndexSnapshot(
+    const std::string &manifest,
+    const std::set<std::string> &available_repository_paths,
+    DirectoryIndexSnapshot &pages, std::string *error = nullptr);
 
 std::string serve(RESPONSE_CALLBACK_ARGS);
 
